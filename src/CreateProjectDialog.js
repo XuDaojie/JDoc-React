81ee1ee456288325075d9b253402b4a8aad6e98d
XuDaojie <xudaojieg@gmail.com>
2017-04-20 22:46:44 +0800

    新建项目-未完成

---
+++ src/CreateProjectDialog.js
@@ -0,0 +1,146 @@
+/**
+ * Created by xdj on 2017/4/20.
+ */
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import LinearProgress from 'material-ui/LinearProgress';
+
+import $ from 'jquery/dist/jquery.min';
+
+const BASE_URL = "http://localhost:8080/JDoc/";
+
+const style = {
+    progressNone: {
+      backgroundColor: '#00bcd4', display: 'none'
+    },
+    progress: {
+      backgroundColor: '#00bcd4', display: 'block'
+    }
+  }
+;
+
+class CreateProjectDialog extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      saveDisabled: false,
+      logining: false,
+      btnText: "保存",
+      progress: style.progressNone,
+      nameErrorMsg: null,
+      pwdErrorMsg: null,
+      username: "",
+      password: "",
+    };
+  }
+
+  static propTypes = {
+    onRequestSuccess: React.PropTypes.func.isRequired,
+  };
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({open: nextProps.open});
+  }
+
+  handleClose() {
+    // this.setState({open: false});
+    // this.setState({open}, this.props.onRequestClose());
+    this.setState({
+      open: false,
+      state: "保存",
+      saveDisabled: false,
+      progress: style.progressNone
+    }, () => this.props.onRequestClose());
+  }
+
+  handleLoginTap() {
+    const nameValue = this.refs.nameInput.input.value;
+    const desValue = this.refs.desInput.input.value;
+    const pwdValue = this.refs.pwdInput.input.value;
+    if (nameValue === '' || !nameValue) {
+      this.setState({nameErrorMsg: "项目名不能为空"});
+      return;
+    }
+
+    $.get(
+      BASE_URL + "create_project.do",
+      {
+        name: nameValue,
+        description: desValue,
+        password: pwdValue,
+      },
+      data => {
+        // console.log(data);
+        if (data.code === 0) {
+          this.setState({btnText: "保存成功"});
+          setTimeout(() => {
+            this.handleClose();
+            // this.setState({}, this.props.onLoginSuccess());
+            this.props.onRequestSuccess();
+          }, 1500);
+        } else {
+          this.setState({nameErrorMsg: data.msg});
+          this.setState({saveDisabled: false});
+          this.setState({progress: style.progressNone});
+        }
+      });
+    this.setState({saveDisabled: true});
+    this.setState({progress: style.progress});
+  }
+
+  handleInputChange(event, newVal) {
+    if (this.state.nameErrorMsg !== null) {
+      this.setState({userErrorMsg: null});
+    }
+    if (this.state.pwdErrorMsg !== null) {
+      this.setState({pwdErrorMsg: null});
+    }
+  }
+
+  render() {
+    return (
+      <Dialog
+        title="新建项目"
+        modal={this.state.saveDisabled}
+        open={this.state.open}
+        onRequestClose={this.handleClose.bind(this)}>
+        <TextField
+          ref="nameInput"
+          floatingLabelText="项目名"
+          errorText={this.state.nameErrorMsg}
+          disabled={this.state.saveDisabled}
+          fullWidth={true}
+          onChange={this.handleInputChange.bind(this)}/>
+        <TextField
+          ref="desInput"
+          floatingLabelText="项目描述"
+          disabled={this.state.saveDisabled}
+          fullWidth={true}
+          multiLine={true}
+          rows={4}/>
+        <TextField
+          ref="pwdInput"
+          floatingLabelText="密码"
+          hintText="非必填"
+          // value={this.state.password}
+          errorText={this.state.pwdErrorMsg}
+          disabled={this.state.saveDisabled}
+          fullWidth={true}/>
+        <RaisedButton label={this.state.btnText} primary={true} style={{marginTop: 16}}
+                      disabled={this.state.saveDisabled}
+                      fullWidth={true}
+                      onTouchTap={this.handleLoginTap.bind(this)}/>
+        <LinearProgress
+          mode="indeterminate" color="#0288D1"
+          style={this.state.progress}/>
+      </Dialog>
+    );
+  };
+
+}
+
+export default CreateProjectDialog;
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -6,6 +6,10 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+import CreateProjectDialog from './CreateProjectDialog';
 
 import $ from 'jquery/dist/jquery.min';
 
@@ -34,6 +38,7 @@ class ProjectList extends React.Component {
     super(props);
     this.state = {
       content: [],
+      openCreate: false,
     };
   }
 
@@ -49,24 +54,47 @@ class ProjectList extends React.Component {
     )
   }
 
+  handleFabClick() {
+    this.setState({openCreate: true});
+  }
+
+  handleCreateClose() {
+    this.setState({openCreate: false});
+  }
+
+  handleCreateProjectSuccess() {
+  }
+
   render() {
-    return <div style={styles.root}>
-      <GridList
-        cellHeight={180}
-        cols={4}
-        style={styles.gridList}>
-        <Subheader>December</Subheader>
-        {this.state.content.map((tile) => (
-          <GridTile
-            key={tile.id}
-            title={tile.name}
-            subtitle={<span>by <b>{tile.name}</b></span>}
-            actionIcon={<IconButton><StarBorder color="white"/>
-            </IconButton>}>
-            <img src="images/test1.png"/>
-          </GridTile>
-        ))}
-      </GridList>
+    return <div>
+      <div style={styles.root}>
+        <GridList
+          cellHeight={180}
+          cols={4}
+          style={styles.gridList}>
+          <Subheader>December</Subheader>
+          {this.state.content.map((tile) => (
+            <GridTile
+              key={tile.id}
+              title={tile.name}
+              subtitle={<span>by <b>{tile.name}</b></span>}
+              actionIcon={<IconButton><StarBorder color="white"/>
+              </IconButton>}>
+              <img src="images/test1.png"/>
+            </GridTile>
+          ))}
+        </GridList>
+      </div>
+      <FloatingActionButton secondary={true}
+                            style={{marginRight: 16, position: 'fixed', right: 64, bottom: 64}}
+                            onTouchTap={this.handleFabClick.bind(this)}>
+        <ContentAdd>新建项目</ContentAdd>
+      </FloatingActionButton>
+
+      <CreateProjectDialog
+        open={this.state.openCreate}
+        onRequestClose={this.handleCreateClose.bind(this)}
+        onRequestSuccess={this.handleCreateProjectSuccess.bind(this)}/>
     </div>;
   }
 }

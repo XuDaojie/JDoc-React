1b7482a9182ca81cab842874043eddd230bbad73
XuDaojie <xudaojieg@gmail.com>
2017-04-24 10:27:13 +0800

    Redux DEMO

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xdj on 2017/4/23.
+ */
+import { combineReducers } from 'redux'
+import testLoginReducer from './testLoginReducer';
+
+const testReducer = combineReducers ({
+  testLoginReducer,
+});
+
+export default testReducer;
